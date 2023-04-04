1d8d5ccece0d4f1babf822822749bb0525f181cb
John-S @ lr03u35bvb <git@suykerbuyk.org>
2023-04-04 20:42:33 +0000

    Fix devmap.sh to included SAS ExosX2/Osprey

--- devmap.sh
+++ devmap.sh
@@ -1,19 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 # vim:ff=unix noati:ts=4:ss=4
 #!/bin/sh
 
 MACH2='wwn-0x6000c500a'
 EVANS='wwn-0x5000c500a'
 TATSU='wwn-0x5000c5009'
+X2SAS='wwn-0x6000c500d'
 
 #for x in $(./devmap.sh  | awk -F ' ' '{print $2}' | grep -v ':'); do dd if=/dev/zero of=/dev/disk/by-id/${x} bs=1M & done
 
 enclosure_sg=$(lsscsi -g \
-   | grep enclos | grep SEAGATE \
+   | grep -i enclos | grep -i SEAGATE \
    | awk '{ print $7 }' | tail -1)
 map_disk_slots() { 
-   for dev in $(ls /dev/disk/by-id/ | grep "$1" | grep -v part) 
-   do
+	for dev in $(ls /dev/disk/by-id/ | grep "$1" | grep -v part) 
+	do
+
        d="/dev/disk/by-id/$dev"
        this_sn=$(sg_vpd --page=0x80 $d \
            | grep 'Unit serial number:' \
@@ -31,7 +33,7 @@ map_disk_slots() {
        echo "  slot=$device_slot $dev sas_addr=$sas_address s/n=$this_sn $kdev"
    done
 }
-for DSK_TYPE in MACH2 EVANS TATSU
+for DSK_TYPE in MACH2 EVANS TATSU X2SAS
 do
 	DSK_PREFIX="${!DSK_TYPE}"
 	echo "$DSK_TYPE ($DSK_PREFIX):"
