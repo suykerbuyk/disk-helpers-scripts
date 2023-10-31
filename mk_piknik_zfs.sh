a00410b9778d9f7d31bfaf905be89dff7e63c749
John Suykerbuyk <john@altlabs.dev>
2023-10-31 13:33:19 -0700

    Added the 5u84 ZFS provisioner script.
    Relies on the output of generate_5u84_topology.sh and the end user
    modifying the .map file to call out the vdev disks.

--- mk_piknik_zfs.sh
+++ mk_piknik_zfs.sh
@@ -6,7 +6,7 @@
 # 18d + 2p + 1s = 21 disk per 288k stripe.
 # 168 child disk / 21 = 8 vdevs across two JBODs.
 #  
-ZPOOL_NAME="altlabs01"
+ZPOOL_NAME="altlabs02"
 ZPOOL_ASHIFT="12"
 ZPOOL_AUTO_TRIM="on"
 ZFS_RECORD_SIZE="$((1024*1024))"
@@ -27,7 +27,7 @@ mk_single_jbod() {
 	ZPOOL_PROPS="-o ashift=${ZPOOL_ASHIFT} -o comment=\"${ZPOOL_COMMENT}\" -o autotrim=${ZPOOL_AUTO_TRIM}"
 	zpool create "${ZPOOL_NAME}" draid2:8d:168c:8s  ${ZPOOL_PROPS} ${ZFS_PROPS} ${BACKING_VDEVS}
 }
-mk_large_jbod(){
+mk_draid_jbods(){
 	. ./disk.groups
 	ZFS_PROPS="-O atime=${ZFS_ATIME} -O recordsize=${ZFS_RECORD_SIZE}\
 	 -O dnodesize=${ZFS_DNODE_SIZE} -O sync=${ZFS_SYNC} -O compression=${ZFS_COMPRESSION}"
@@ -38,4 +38,44 @@ mk_large_jbod(){
 		draid2:8d:84c:4s ${DSKGRP3}\
 		draid2:8d:84c:4s ${DSKGRP4}
 }
-mk_large_jbod
+mk_4_5u84_raidz2() {
+	. ./altlabs02-10disk-raidz2.map
+	ZFS_PROPS="-O atime=${ZFS_ATIME} -O recordsize=${ZFS_RECORD_SIZE}\
+	 -O dnodesize=${ZFS_DNODE_SIZE} -O sync=${ZFS_SYNC} -O compression=${ZFS_COMPRESSION}"
+	ZPOOL_PROPS="-o ashift=${ZPOOL_ASHIFT} -o comment=\"${ZPOOL_COMMENT}\" -o autotrim=${ZPOOL_AUTO_TRIM}"
+	zpool create "${ZPOOL_NAME}" ${ZPOOL_PROPS} ${ZFS_PROPS}\
+		raidz2 ${RAIDZ2_10_DSKGRP00}\
+		raidz2 ${RAIDZ2_10_DSKGRP01}\
+		raidz2 ${RAIDZ2_10_DSKGRP02}\
+		raidz2 ${RAIDZ2_10_DSKGRP03}\
+		raidz2 ${RAIDZ2_10_DSKGRP04}\
+		raidz2 ${RAIDZ2_10_DSKGRP05}\
+		raidz2 ${RAIDZ2_10_DSKGRP06}\
+		raidz2 ${RAIDZ2_10_DSKGRP07}\
+		raidz2 ${RAIDZ2_10_DSKGRP08}\
+		raidz2 ${RAIDZ2_10_DSKGRP09}\
+		raidz2 ${RAIDZ2_10_DSKGRP10}\
+		raidz2 ${RAIDZ2_10_DSKGRP11}\
+		raidz2 ${RAIDZ2_10_DSKGRP12}\
+		raidz2 ${RAIDZ2_10_DSKGRP13}\
+		raidz2 ${RAIDZ2_10_DSKGRP14}\
+		raidz2 ${RAIDZ2_10_DSKGRP15}\
+		raidz2 ${RAIDZ2_10_DSKGRP16}\
+		raidz2 ${RAIDZ2_10_DSKGRP17}\
+		raidz2 ${RAIDZ2_10_DSKGRP18}\
+		raidz2 ${RAIDZ2_10_DSKGRP19}\
+		raidz2 ${RAIDZ2_10_DSKGRP20}\
+		raidz2 ${RAIDZ2_10_DSKGRP21}\
+		raidz2 ${RAIDZ2_10_DSKGRP22}\
+		raidz2 ${RAIDZ2_10_DSKGRP23}\
+		raidz2 ${RAIDZ2_10_DSKGRP24}\
+		raidz2 ${RAIDZ2_10_DSKGRP25}\
+		raidz2 ${RAIDZ2_10_DSKGRP26}\
+		raidz2 ${RAIDZ2_10_DSKGRP27}\
+		raidz2 ${RAIDZ2_10_DSKGRP28}\
+		raidz2 ${RAIDZ2_10_DSKGRP29}\
+		raidz2 ${RAIDZ2_10_DSKGRP30}\
+		raidz2 ${RAIDZ2_10_DSKGRP31}\
+		spare ${RAIDZ2_10_SPARES}
+}
+mk_4_5u84_raidz2
