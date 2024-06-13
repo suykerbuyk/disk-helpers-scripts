19c2234a7f34ed5f3dc8ded80e8964f0e91fc5a9
john-s <john.suykerbuyk@web3mine.io>
2024-06-13 13:44:39 -0700

    Updates to the inventory methods.

--- build.ict.jbod.sh
+++ build.ict.jbod.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+set -e
 
 HNAME="$(uname -n)"
-OFILE="${HNAME}-devmap.txt"
+DEV_FILE="${HNAME}-devmap.txt"
+ENC_FILE="${HNAME}-encmap.txt"
 
-sudo ./devmap.sh | sort | tee "${OFILE}"
-sudo "${OFILE}" /etc/zfs/vdev_id.conf 
-sudo udevadm trigger
-
-cat "${OFILE}" | awk '{print $2}' | awk -F '_' '{print $1}' | sort -u | tee "${HNAME}-encmap.txt"
+build_devmap() {
+	./devmap.sh | sort | tee "${DEV_FILE}"
+	cp "${DEV_FILE}" /etc/zfs/vdev_id.conf 
+	udevadm trigger
+}
+build_encmap() {
+cat "${DEV_FILE}" | awk '{print $2}' | awk -F '_' '{print $1}' | sort -u | tee "${ENC_FILE}"
+for ENC in $(cat "${ENC_FILE}")
+do
+	cat "${DEV_FILE}" | grep "${ENC}" >"${HNAME}-${ENC}-devmap.txt"
+done
+}
+build_devmap
+build_encmap
