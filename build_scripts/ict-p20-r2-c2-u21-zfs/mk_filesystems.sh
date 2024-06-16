3e0dd07b9929ffc0e62518f816d55c8f6776cd29
john-s <john.suykerbuyk@web3mine.io>
2024-06-15 19:39:54 -0700

    Commit first pass generated provitioning scripts for ICT JBOD Rack 2

---
+++ build_scripts/ict-p20-r2-c2-u21-zfs/mk_filesystems.sh
@@ -0,0 +1,12 @@
+zfs create -o canmount=on -o mountpoint=/storage-a ZPOOL_ict-p20-r2-c2-u21-zfs-a/storage-a
+zfs create -o canmount=on -o recordsize=1M         ZPOOL_ict-p20-r2-c2-u21-zfs-a/storage-a/sealed
+zfs create -o canmount=on -o recordsize=1M         ZPOOL_ict-p20-r2-c2-u21-zfs-a/storage-a/unsealed
+zfs create -o canmount=on -o recordsize=256k       ZPOOL_ict-p20-r2-c2-u21-zfs-a/storage-a/cache
+zfs create -o canmount=on -o recordsize=256k       ZPOOL_ict-p20-r2-c2-u21-zfs-a/storage-a/update
+zfs create -o canmount=on -o recordsize=256k       ZPOOL_ict-p20-r2-c2-u21-zfs-a/storage-a/update-cache
+zfs create -o canmount=on -o mountpoint=/storage-b ZPOOL_ict-p20-r2-c2-u21-zfs-b/storage-b
+zfs create -o canmount=on -o recordsize=1M         ZPOOL_ict-p20-r2-c2-u21-zfs-b/storage-b/sealed
+zfs create -o canmount=on -o recordsize=1M         ZPOOL_ict-p20-r2-c2-u21-zfs-b/storage-b/unsealed
+zfs create -o canmount=on -o recordsize=256k       ZPOOL_ict-p20-r2-c2-u21-zfs-b/storage-b/cache
+zfs create -o canmount=on -o recordsize=256k       ZPOOL_ict-p20-r2-c2-u21-zfs-b/storage-b/update
+zfs create -o canmount=on -o recordsize=256k       ZPOOL_ict-p20-r2-c2-u21-zfs-b/storage-b/update-cache
